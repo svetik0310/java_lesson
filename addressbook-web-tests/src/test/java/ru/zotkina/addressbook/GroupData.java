bccd6e7a87f45c07357c276c2b3df284856e2e5c
Света <svetiksoleil@gmail.com>
2018-03-12 22:39:22 +0300

    Рефакторинг для создания группы

---
+++ addressbook-web-tests/src/test/java/ru/zotkina/addressbook/GroupData.java
@@ -0,0 +1,25 @@
+package ru.zotkina.addressbook;
+
+public class GroupData {
+    private final String groupname;
+    private final String header;
+    private final String footer;
+
+    public GroupData(String groupname, String header, String footer) {
+        this.groupname = groupname;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public String getGroupname() {
+        return groupname;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+}
