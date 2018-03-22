51f6527ab22254bde3a4633517d4548a43f7317f
Света <svetiksoleil@gmail.com>
2018-03-23 00:32:21 +0300

    Домашнее задание 8_v1

--- addressbook-web-tests/src/test/java/ru/zotkina/addressbook/tests/ContractEditTests.java
+++ addressbook-web-tests/src/test/java/ru/zotkina/addressbook/tests/ContractEditTests.java
@@ -2,11 +2,16 @@ package ru.zotkina.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.zotkina.addressbook.model.ContactData;
+import ru.zotkina.addressbook.model.GroupData;
 
 public class ContractEditTests extends TestBase{
     @Test
     public void testContactUpdate() {
         if(!app.getContractHelper().isContactExist()) {
+            app.getNavigationHelper().gotoGroupPage();
+            if(!app.getGroupHelper().isGroupExist()) {
+                app.getGroupHelper().createGroup(new GroupData("test1A", "test2A", "test3A"));
+            }
             app.getContractHelper().createContact(new ContactData("Василий", "Иванович", "Пупкин", "Vasya", "title", "company", "address", "222", "333", "444", "555", "3445", "432434", "2423424", "343543545", "test1A"), false);
             app.getNavigationHelper().returnToHomePage();
         }
