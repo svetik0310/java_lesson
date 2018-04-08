dee9b32dba7ae96075f540e6f00998410b8252ea
Света <svetiksoleil@gmail.com>
2018-04-09 00:54:32 +0300

    Домашнее задание 14

--- addressbook-web-tests/src/test/java/ru/zotkina/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/zotkina/addressbook/tests/TestBase.java
@@ -9,7 +9,8 @@ import ru.zotkina.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser",BrowserType.FIREFOX));
 
     @BeforeSuite
     public void setUp() throws Exception {
