1d4953bcacc41f256bb65339982cc7ffda158775
Света <svetiksoleil@gmail.com>
2018-05-08 00:49:16 +0300

    10_2 удаленный браузер

--- addressbook-web-tests/src/test/java/ru/zotkina/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/zotkina/addressbook/appmanager/ApplicationManager.java
@@ -6,10 +6,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -35,16 +38,24 @@ public class ApplicationManager {
 
         dbHelper=new DbHelper();
 
-        if (browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        }
-        else if (browser.equals(BrowserType.CHROME))
+        if("".equals(properties.getProperty("selenium.server")))
         {
-            wd = new ChromeDriver();
+            if (browser.equals(BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+            }
+            else if (browser.equals(BrowserType.CHROME))
+            {
+                wd = new ChromeDriver();
+            }
+            else if (browser.equals(BrowserType.IE))
+            {
+                wd = new InternetExplorerDriver();
+            }
         }
-        else if (browser.equals(BrowserType.IE))
-        {
-            wd = new InternetExplorerDriver();
+        else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd=new RemoteWebDriver(new URL(properties.getProperty("selenium.server")),capabilities);
         }
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.BaseUrl"));
